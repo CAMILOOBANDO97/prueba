67362fd998eb6ef25cdb8029b43705f7ed84451a
Jc <jc.obando10@uniandes.edu.co>
2019-04-11 03:38:06 +0000

    otro ej.2

--- 2.cpp
+++ 2.cpp
@@ -3,12 +3,13 @@
 #include <math.h>
 using std::cout;
 using std::endl;
-
+//defino la función que me dice si los lados del triángulo son o no de un t.rectángulo
 void es_un_t_rectangulo(int h, int a , int b);
 
 
 int main()
 {
+    //llamo la función y pruebo el primer triángulo
     cout << "El triángulo con los lados 3, 4, 5 " << " ";
     es_un_t_rectangulo(5, 4 , 3);
     cout << "El triángulo con los lados 3, 4, 6 " << " ";
@@ -16,6 +17,7 @@ int main()
     
 }
 
+//función que me dice si esa o no un t.rectangulo
 void es_un_t_rectangulo(int h, int a , int b)
 {
     if(pow(h,2) == pow(a,2) + pow(b,2)){
